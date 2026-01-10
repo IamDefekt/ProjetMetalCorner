81a9179838e3ce8a69469e9ba7512403cd4d8842
Defekt <iamdefekt@gmail.com>
2026-01-10 03:51:02 +0100

    modif sidebar admin

---
+++ view/components/sidebar.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/style/components/sidebar.css">
+</head>
+<body>
+    
+    <nav class="sidebar">
+        <ul>
+            <li><a href="">Dashboard</a></li>
+            <li><a href="">Concerts</a></li>
+            <li><a href="">Articles </a></li>
+            <li><a href="">Propositions </a></li>
+            <li><a href="">Statistiques </a></li>
+        </ul>
+
+        <div class="btn">
+            <a class="logout" href="/view/logout.php">Se déconnecter</a>
+        </div>
+    </nav>
+
+
+</body>
+</html>
