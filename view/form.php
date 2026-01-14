94db8bd218e8e6d5c411f48136c1905e72134179
Defekt <iamdefekt@gmail.com>
2026-01-15 00:11:52 +0100

    modif mise en page

--- view/form.php
+++ view/form.php
@@ -14,27 +14,27 @@
 
     <main>
 
-        <h1>Nous Contacter</h1>
+        <h1>Formulaire de contact</h1>
     
         <form id="form">
 
-            <label for="artistName" style="width:153px">Nom du/des artiste(s)</label>
+            <label for="artistName" style="width:150px">Nom du/des artiste(s)</label>
             <input placeholder="Obligatoire" type="text" id="artistName" tabindex="1" required> 
 
             <span id='errorArtist' aria-live="polite"></span>
 
-            <label for="dateConcert" style="width:115px">Date du concert</label>
+            <label for="dateConcert" style="width:117px">Date du concert</label>
             <input placeholder="Obligatoire" type="date" id="dateConcert" tabindex="2" required>
 
             <span id='errorDate' aria-live="polite"></span>
 
-            <label for="LieuConcert" style="width:110px">Lieu du concert</label>
+            <label for="LieuConcert" style="width:112px">Lieu du concert</label>
             <input placeholder="Obligatoire" type="text" id="LieuConcert" tabindex="3" required>
 
             <span id='errorPlace' aria-live="polite"></span>
 
-            <label for="feedback" style="max-width:127px">Un p'tit message ?</label>
-            <textarea name="feedback"></textarea>
+            <label for="feedback" style="max-width:121px">Un p'tit message ?</label>
+            <textarea name="feedback" tabindex="4"></textarea>
 
             <button class="btn_red" id="btn" type="button"/>Envoyer</button>
 
