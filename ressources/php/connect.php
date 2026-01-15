02178e22e74b77f0f1145c92df9eb574c363b30f
Defekt <iamdefekt@gmail.com>
2026-01-15 16:21:29 +0100

    maj pour connexion et creation user

--- ressources/php/connect.php
+++ ressources/php/connect.php
@@ -1,33 +1,32 @@
-<?php 
+<?php
 
-    require_once 'configdb.php';
+require_once 'configdb.php';
 
 class Connect {
 
-    private string $truc = __DIR__ . '/../conf/metalcorner.conf';
+    private PDO $pdo;
     private ConfigDB $cdb;
-
+    private string $truc = __DIR__ . '/../conf/metalcorner.conf';
+    
     public function __construct() {
-        $this->cdb = new configdb($this->truc);
+        $this->cdb = new ConfigDB($this->truc);
         $this->cdb->loadFromFile();
     }
 
-        public function connect(): PDO {
+    public function connect(): PDO {
+    $dsn = 'mysql:host=' . $this->cdb->getHost() . ';dbname=' . $this->cdb->getDB() . ';charset=' . $this->cdb->getCharset();
+    $options = [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES   => false,
+    ];
 
-            $dsn = "mysql:host={$this->cdb->getHost()}; port={$this->cdb->getPort()};dbname={$this->cdb->getDB()};charset={$this->cdb->getCharset()}";
-
-            $options = [
-            PDO::ATTR_ERRMODE               =>   PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE    =>   PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES      =>   false,
-        ];
-
-        try {
-            $pdo = new PDO($dsn, $this->cdb->getUser(), $this->cdb->getMdp(), $options);
-        } catch (PDOException $e) {
-            throw new PDOException($e->getMessage(), (int)$e->getCode());
-        } return $pdo;
-
-}
+    try {
+        $this->pdo = new PDO($dsn, $this->cdb->getUser(), $this->cdb->getMdp(), $options);
+        return $this->pdo;
+    } catch (PDOException $e) {
+        die('Erreur connexion BDD : ' . $e->getMessage());
+        }
+    }
 
 }
