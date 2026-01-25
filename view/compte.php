3ba443e008d8c8931deb1bd7e9e5b0409ef9a82d
Defekt <iamdefekt@gmail.com>
2026-01-25 13:22:20 +0100

    modif icone groupes suivis

--- view/compte.php
+++ view/compte.php
@@ -13,14 +13,14 @@
     <main>
 
         <h1>Mon Compte</h1>
-
+<!-- 
         <div class="ancre">
             <h3><a href="#infos">Informations personnelles</a></h3> 
             <h3><a href="#alertes">Mes alertes</a></h3>
             <h3><a href="#registered_events">évènements enregistrés</a></h3>
-        </div>
+        </div> 
 
-        <span class="sep"></span>
+        <span class="sep"></span>-->
 
         <div id="infos">
 
@@ -101,7 +101,13 @@
                         <img src="/ressources/media/bands/Architects.png" alt="Photo du groupe Architects">
                         <p>Architects</p>
                     </div>
-                    <a class="btn_red" id="follow" href="">Suivis</a>
+                    <a class="btn_red" id="follow" href="">
+
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bell-fill" viewBox="0 0 16 16">
+                        <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2m.995-14.901a1 1 0 1 0-1.99 0A5 5 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901"/>
+                        </svg>
+
+                    </a>
                 </div>
 
                 <div class="band">
@@ -109,7 +115,11 @@
                         <img src="/ressources/media/bands/BadOmens.jpg" alt="Photo du groupe Bad Omens">
                         <p>Bad Omens</p>
                     </div>
-                    <a class="btn_red"  id="follow" href="">Suivis</a>
+                    <a class="btn_red"  id="follow" href="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bell-fill" viewBox="0 0 16 16">
+                        <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2m.995-14.901a1 1 0 1 0-1.99 0A5 5 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901"/>
+                        </svg>
+                    </a>
                 </div>
 
                 <div class="band">
@@ -117,7 +127,11 @@
                         <img src="/ressources/media/bands/BlackVeilBrides.jpg" alt="Photo du groupe Black Veil Brides">
                         <p>Black Veil Brides</p>
                     </div>
-                    <a class="btn_red"  id="follow" href="">Suivis</a>
+                    <a class="btn_red"  id="follow" href="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bell-fill" viewBox="0 0 16 16">
+                        <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2m.995-14.901a1 1 0 1 0-1.99 0A5 5 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901"/>
+                        </svg>
+                    </a>
                 </div>
 
                 <div class="band">
@@ -125,7 +139,11 @@
                         <img src="/ressources/media/bands/BMTH2.jpg" alt="Photo du groupe Bring me the Horizon">
                         <p>Bring me the Horizon</p>
                     </div>
-                    <a class="btn_red" id="follow"  href="">Suivis</a>
+                    <a class="btn_red" id="follow"  href="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bell-fill" viewBox="0 0 16 16">
+                        <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2m.995-14.901a1 1 0 1 0-1.99 0A5 5 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901"/>
+                        </svg>
+                    </a>
                 </div>
 
                             <div class="band">
@@ -133,7 +151,11 @@
                         <img src="/ressources/media/bands/Deftones.jpg" alt="Photo du groupe Deftones">
                         <p>Deftones</p>
                     </div>
-                    <a class="btn_red" id="follow" href="">Suivis</a>
+                    <a class="btn_red" id="follow" href="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bell-fill" viewBox="0 0 16 16">
+                        <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2m.995-14.901a1 1 0 1 0-1.99 0A5 5 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901"/>
+                        </svg>
+                    </a>
                 </div>
 
                 <div class="band">
@@ -141,7 +163,11 @@
                         <img src="/ressources/media/bands/ElectricCallboy.webp" alt="Photo du groupe Electric Callboy">
                         <p>Electric Callboy</p>
                     </div>
-                    <a class="btn_red" id="follow" href="">Suivis</a>
+                    <a class="btn_red" id="follow" href="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bell-fill" viewBox="0 0 16 16">
+                        <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2m.995-14.901a1 1 0 1 0-1.99 0A5 5 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901"/>
+                        </svg>
+                    </a>
                 </div>
 
                 <div class="band">
@@ -149,7 +175,11 @@
                         <img src="/ressources/media/bands/Eisbrecher.jpg" alt="Photo du groupe Eisbrecher">
                         <p>Eisbrecher</p>
                     </div>
-                    <a class="btn_red" id="follow" href="">Suivis</a>
+                    <a class="btn_red" id="follow" href="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bell-fill" viewBox="0 0 16 16">
+                        <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2m.995-14.901a1 1 0 1 0-1.99 0A5 5 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901"/>
+                        </svg>
+                    </a>
                 </div>
 
                 <div class="band">
@@ -157,7 +187,11 @@
                         <img src="/ressources/media/bands/FIR.jpg" alt="Photo du groupe Falling in Reverse">
                         <p>Falling in Reverse</p>
                     </div>
-                    <a class="btn_red" id="follow" href="">Suivis</a>
+                    <a class="btn_red" id="follow" href="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bell-fill" viewBox="0 0 16 16">
+                        <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2m.995-14.901a1 1 0 1 0-1.99 0A5 5 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901"/>
+                        </svg>
+                    </a>
                 </div>
 
                 <div class="band">
@@ -165,7 +199,11 @@
                         <img src="/ressources/media/bands/korn.jpg" alt="Photo du groupe KoRn">
                         <p>Korn</p>
                     </div>
-                    <a class="btn_red" id="follow" href="">Suivis</a>
+                    <a class="btn_red" id="follow" href="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bell-fill" viewBox="0 0 16 16">
+                        <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2m.995-14.901a1 1 0 1 0-1.99 0A5 5 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901"/>
+                        </svg>
+                    </a>
                 </div>
 
                 <div class="band">
@@ -173,7 +211,11 @@
                         <img src="/ressources/media/bands/LinkinPark.jpg" alt="Photo du groupe Linkin Park">
                         <p>Linkin Park</p>
                     </div>
-                    <a class="btn_red" id="follow" href="">Suivis</a>
+                    <a class="btn_red" id="follow" href="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bell-fill" viewBox="0 0 16 16">
+                        <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2m.995-14.901a1 1 0 1 0-1.99 0A5 5 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901"/>
+                        </svg>
+                    </a>
                 </div>
 
                 <div class="band">
@@ -181,7 +223,11 @@
                         <img src="/ressources/media/bands/LornaShore.jpg" alt="Photo du groupe Lorna Shore">
                         <p>Lorna Shore</p>
                     </div>
-                    <a class="btn_red" id="follow" href="">Suivis</a>
+                    <a class="btn_red" id="follow" href="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bell-fill" viewBox="0 0 16 16">
+                        <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2m.995-14.901a1 1 0 1 0-1.99 0A5 5 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901"/>
+                        </svg>
+                    </a>
                 </div>
 
                 <div class="band">
@@ -189,7 +235,11 @@
                         <img src="/ressources/media/bands/MtS.jpg" alt="Photo du groupe Make them Suffer">
                         <p>Make them Suffer</p>
                     </div>
-                    <a class="btn_red" id="follow" href="">Suivis</a>
+                    <a class="btn_red" id="follow" href="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bell-fill" viewBox="0 0 16 16">
+                        <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2m.995-14.901a1 1 0 1 0-1.99 0A5 5 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901"/>
+                        </svg>
+                    </a>
                 </div>
 
                 <div class="band">
@@ -197,7 +247,11 @@
                         <img src="/ressources/media/bands/mm.jpg" alt="Photo du groupe de Marilyn Manson">
                         <p>Marilyn Manson</p>
                     </div>
-                    <a class="btn_red" id="follow" href="">Suivis</a>
+                    <a class="btn_red" id="follow" href="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bell-fill" viewBox="0 0 16 16">
+                        <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2m.995-14.901a1 1 0 1 0-1.99 0A5 5 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901"/>
+                        </svg>
+                    </a>
                 </div>
 
                 <div class="band">
@@ -205,7 +259,11 @@
                         <img src="/ressources/media/bands/MIW.webp" alt="Photo du groupe Motionless in White">
                         <p>Motionless in White</p>
                     </div>
-                    <a class="btn_red" id="follow" href="">Suivis</a>
+                    <a class="btn_red" id="follow" href="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bell-fill" viewBox="0 0 16 16">
+                        <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2m.995-14.901a1 1 0 1 0-1.99 0A5 5 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901"/>
+                        </svg>
+                    </a>
                 </div>
 
             </div>
