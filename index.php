0b016d5b0c3f95d8749c97d9bed33922118cc1b0
Defekt <iamdefekt@gmail.com>
2026-09-07 20:26:04 +0200

    correction indentation

--- index.php
+++ index.php
@@ -17,54 +17,53 @@
             <h1>L'agenda participatif des concerts et festivals métal en Rhône-Alpes</h1>
         </section>
 
-    <span class="sep"></span>
+        <span class="sep"></span>
 
+        <section class="cards">
 
-    <section class="cards">
+            <div class="show">
 
-        <div class="show">
+                <h2>Prochains évènements</h2>
 
-            <h2>Prochains évènements</h2>
+                <div class="live">
 
-            <div class="live">
+                    <a href="#">20/01 - Electric Callboy @LDLC Arena</a>
+                    <a href="">12/03 - Gojira @Groupama Stadium</a>
+                    <a href="">18/03 - Amenra @Rock'n'Eat</a>
+                    <a href="">22/03 - Loudblast @Radiant</a>
+                    <a href="">25/03 - Perturbator @Ninkasi Kao</a>
 
-                <a href="#">20/01 - Electric Callboy @LDLC Arena</a>
-                <a href="">12/03 - Gojira @Groupama Stadium</a>
-                <a href="">18/03 - Amenra @Rock'n'Eat</a>
-                <a href="">22/03 - Loudblast @Radiant</a>
-                <a href="">25/03 - Perturbator @Ninkasi Kao</a>
+                </div>
 
-            </div>
-
-            <a class="btn_red" href="#">Voir tout les évènements</a>
-
-        </div>
-
-        <div class="lastAjout">
-
-            <h2>Derniers évènements ajoutés</h2>
-
-            <div class="live">
-
-                <a href="#">20/01 - Electric Callboy @LDLC Arena</a>
-                <a href="">12/03 - Gojira @Groupama Stadium</a>
-                <a href="">18/03 - Amenra @Rock'n'Eat</a>
-                <a href="">22/03 - Loudblast @Radiant</a>
-                <a href="">25/03 - Perturbator @Ninkasi Kao</a>
+                <a class="btn_red" href="#">Voir tout les évènements</a>
 
             </div>
 
-        </div>
+            <div class="lastAjout">
 
-    </section>
+                <h2>Derniers évènements ajoutés</h2>
 
-    <section class="cta">
+                <div class="live">
 
-        <h3>Un concert ou un festival n'est pas listé ? Dis-le nous !</h3>
+                    <a href="#">20/01 - Electric Callboy @LDLC Arena</a>
+                    <a href="">12/03 - Gojira @Groupama Stadium</a>
+                    <a href="">18/03 - Amenra @Rock'n'Eat</a>
+                    <a href="">22/03 - Loudblast @Radiant</a>
+                    <a href="">25/03 - Perturbator @Ninkasi Kao</a>
 
-        <a class="btn_white" href="/view/form.php">Proposer un évènement</a>
+                </div>
 
-    </section>
+            </div>
+
+        </section>
+
+        <section class="cta">
+
+            <h3>Un concert ou un festival n'est pas listé ? Dis-le nous !</h3>
+
+            <a class="btn_white" href="/view/form.php">Proposer un évènement</a>
+
+        </section>
         
     </main>
    
